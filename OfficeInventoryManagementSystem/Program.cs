260b93248df924a148781e42482ebfc7ab2d2981
RegisteredLerner <gamedevado@gmail.com>
2025-09-10 14:04:53 +0200

    Succeded project about lists

---
+++ OfficeInventoryManagementSystem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class InventoryManagement
+{
+    public static void Main(string[] args)
+    {
+        // Your code goes here
+        List<string> inventoryList = new List<string>();
+        inventoryList.Add("Printer");
+        inventoryList.Add("Laptop");
+        inventoryList.Add("Desk Chair");
+        inventoryList.Add("Monitor");
+        inventoryList.Add("Keyboard");
+        Console.WriteLine(inventoryList.Count);
+        bool hasDeskChair = inventoryList.Contains("Desk Chair");
+        Console.WriteLine(hasDeskChair);
+        bool removed = inventoryList.Remove("Printer");
+        Console.WriteLine(removed);
+        foreach (string inv in inventoryList)
+        {
+            Console.WriteLine(inv);
+        }
+        List<string> newItems = new List<string> { "Mouse", "Desk Lamp" };
+        inventoryList.AddRange(newItems);
+        inventoryList.RemoveRange(4, 2);
+        List<string> topInventory = inventoryList.GetRange(0, 3);
+        foreach (string top in topInventory)
+        {
+            Console.WriteLine(top);
+        }
+
+  }
+}
